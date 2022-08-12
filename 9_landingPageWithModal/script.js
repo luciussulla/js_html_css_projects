ca660e7bdf66de17dd7817b4131c17f66bb68021
luciussulla <maciek.kowalczyk@yandex.com>
2022-08-12 17:58:21 +0200

    floor and table finished

--- 9_landingPageWithModal/script.js
+++ 9_landingPageWithModal/script.js
@@ -1,3 +1,5 @@
+
+
 const signupModal = document.querySelector('.signup-form-wrapper');
 const loginModal = document.querySelector('.login-form-wrapper');
 const signupBtn = document.querySelector('.signup-btn'); 
